a08c8279c2ae1d987eb063b968b2252dbd9e6a87
rs691 <robert.stewart.m@outlook.com>
2025-12-08 15:20:07 -0600

    added other files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,28 +1,29 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.myapp"
-    compileSdk = 33 // A stable, common version
+    namespace = "com.example.bellevueopoly"
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        applicationId = "com.example.myapp"
-        minSdk = 21 // A common minimum SDK
-        targetSdk = 33 // Matching compileSdk
+        applicationId = "com.example.bellevueopoly"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -36,4 +37,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
 }
